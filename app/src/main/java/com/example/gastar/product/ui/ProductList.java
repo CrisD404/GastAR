4c6e2b924194df27d20e5f0041fb106c4be2e444
CrisD404 <cristiandelcanto880@gmail.com>
2024-09-15 18:30:16 -0300

    feat: add product button

--- app/src/main/java/com/example/gastar/product/ui/ProductList.java
+++ app/src/main/java/com/example/gastar/product/ui/ProductList.java
@@ -1,49 +1,63 @@
 package com.example.gastar.product.ui;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.gastar.R;
 import com.example.gastar.product.entity.Product;
 
 import java.util.List;
 
-public class ProductList extends ArrayAdapter<Product> {
-    private final Context context;
+public class ProductList extends RecyclerView.Adapter<ProductList.ViewHolder> {
     private final List<Product> products;
-    private final int resource;
 
-    public ProductList(@NonNull Context context, @NonNull List<Product> products) {
-        super(context, R.layout.product_item, products);
-        this.resource = R.layout.product_item;
-        this.context = context;
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView nameLabel;
+        private final TextView priceLabel;
+
+        public ViewHolder(View view) {
+            super(view);
+            nameLabel = view.findViewById(R.id.productName);
+            priceLabel = view.findViewById(R.id.productPrice);
+        }
+
+        public TextView getNameLabel() {
+            return nameLabel;
+        }
+
+        public TextView getPriceLabel() {
+            return priceLabel;
+        }
+    }
+
+    public ProductList(List<Product> products) {
         this.products = products;
     }
 
-    @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View view = convertView;
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.product_item, viewGroup, false);
 
-        if (view == null) {
-            view = LayoutInflater.from(context).inflate(resource, null);
-        }
+        return new ViewHolder(view);
+    }
 
-        Product product = products.get(position);
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        viewHolder.getNameLabel().setText(products.get(position).getName());
+        viewHolder.getPriceLabel().setText(products.get(position).getPrice().toString());
+    }
 
-        TextView nameLabel = view.findViewById(R.id.productName);
-        TextView priceLabel = view.findViewById(R.id.productPrice);
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
 
-        nameLabel.setText(product.getName());
-        priceLabel.setText(product.getPrice().toString());
-
-        return view;
+    public Double getFullPrice() {
+        return products.stream().mapToDouble(Product::getTotalPrice).sum();
     }
 }
