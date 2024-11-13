e575fd3ffb9bd29357fb354a4776905165ec4bda
Solaris-99 <e.cifuentes996@gmail.com>
2024-11-13 11:17:45 -0300

    feat: creation of files and basic infrastructure for view

---
+++ app/src/main/java/com/example/gastar/person/AddPersonFragment.java
@@ -0,0 +1,23 @@
+package com.example.gastar.person;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.gastar.R;
+
+public class AddPersonFragment  extends Fragment {
+
+    public AddPersonFragment(){
+        super(R.layout.add_product_person_fields);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/gastar/product/AddProductFragment.java
@@ -0,0 +1,22 @@
+package com.example.gastar.product;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.gastar.R;
+
+public class AddProductFragment extends Fragment {
+
+    public AddProductFragment(){
+        super(R.layout.add_product_product_fields);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
+
+    }
+
+}
